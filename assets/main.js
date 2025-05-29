5f60df9a9dbbd15e5a94ac85ddf49819d3a0862e
mpitfield <mylespitfield10@outlook.com>
2025-05-29 23:04:41 +0100

    Create main.js

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+<script>
+function createAlbumReview(title, body) {
+    let album = "xxx";
+    fetch("https://rythmic-backend.vercel.app/api/submit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: title, body: body })
+    })
+        .then(res => res.json())
+        .then(data => console.log("Success:", data))
+        .catch(err => console.error("Error:", err));
+}
+</script>
