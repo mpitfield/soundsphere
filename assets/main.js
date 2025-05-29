281f0a6f2b12bddee131afd83f281d4156be00e0
mpitfield <mylespitfield10@outlook.com>
2025-05-29 23:05:01 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,7 @@ function createAlbumReview(title, body) {
     fetch("https://rythmic-backend.vercel.app/api/submit", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ title: title, body: body })
+        body: JSON.stringify({ title: title, body: body, album: album })
     })
         .then(res => res.json())
         .then(data => console.log("Success:", data))
